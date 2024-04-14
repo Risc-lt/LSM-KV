e4dce0ccc705716870c2d7611f8b5afcd7c373ea
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-04-14 20:16:47 +0800

    feat: sstheader

---
+++ sstheader.cc
@@ -0,0 +1,85 @@
+#include "sstheader.h"
+
+// Constructor
+SSTheader::SSTheader(std::string path, uint32_t offset) {
+    if(readFile(path, offset) != 0) {
+        std::cerr << "[Error] Failure of constructing sstheader" << std::endl;
+    }
+}
+
+// Load the SST header from a file
+int SSTheader::readFile(std::string path, uint32_t offset) {
+    std::ifstream inFile(path, std::ios::in | std::ios::binary);
+    if(!inFile)
+        return -1;
+
+    // Move the file pointer to the offset
+    inFile.seekg(0,std::ios::end);
+    // Get the file size
+    uint32_t fileLimit = inFile.tellg();
+
+    // Check if the offset is out of bounds
+    if(offset > fileLimit || offset + sizeof(SSTheader) > fileLimit){
+        inFile.close();
+        return -2;
+    }
+    
+    inFile.clear();
+    
+    // Move the file pointer to the offset
+    inFile.seekg(offset,std::ios::beg);
+
+    // Read the SST header
+    inFile.read((char*)&timeStamp, sizeof(timeStamp));
+    inFile.read((char*)&keyValNum, sizeof(keyValNum));
+    inFile.read((char*)&minKey, sizeof(minKey));
+    inFile.read((char*)&maxKey, sizeof(maxKey));
+
+    // Close the file
+    inFile.close();
+    return 0;
+}
+
+// Save the SST header to a file
+uint32_t SSTheader::writeToFile(std::string path, uint32_t offset) {
+    bool isFileExists = false;
+    uint32_t fileLimit = 0;
+    std::ifstream inFile(path, std::ios::in | std::ios::binary);
+
+    // Check if the file exists
+    if(inFile){
+        isFileExists = true;
+        inFile.seekg(0, std::ios::end);
+        fileLimit = inFile.tellg();
+        inFile.close();
+    }
+
+    // Create a new file if it does not exist
+    if(!isFileExists){
+        offset = 0;
+        std::fstream createFile(path, std::ios::out | std::ios::binary);
+        createFile.close();
+    } else if (isFileExists && offset > fileLimit) {
+        // If the offset is out of bounds, set the offset to the end of the file
+        offset = fileLimit;
+    }
+
+    // Open the file in read/write mode
+    std::fstream outFile(path, std::ios::out | std::ios::in | std::ios::binary);
+
+    if(!outFile)
+        return -1;
+
+    // Move the file pointer to the offset
+    outFile.seekp(offset, std::ios::beg);
+
+    // Write the SST header
+    outFile.write((char*)&timeStamp, sizeof(timeStamp));
+    outFile.write((char*)&keyValNum, sizeof(keyValNum));
+    outFile.write((char*)&minKey, sizeof(minKey));
+    outFile.write((char*)&maxKey, sizeof(maxKey));
+
+    // Close the file
+    outFile.close();
+    return offset;
+}
---
+++ sstheader.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class SSTheader {
+public:
+    uint64_t timeStamp;
+    uint64_t keyValNum;
+    uint64_t minKey;
+    uint64_t maxKey;
+
+    // Constructor
+    SSTheader(){};
+    SSTheader(std::string path, uint32_t offset);
+
+    // Destructor
+    ~SSTheader(){};
+
+    // Load the SST header from a file
+    int readFile(std::string path, uint32_t offset);
+
+    // Save the SST header to a file
+    uint32_t writeToFile(std::string path, uint32_t offset);
+};
